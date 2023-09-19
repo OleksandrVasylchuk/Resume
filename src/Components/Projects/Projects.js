126820cd2c6726bce4d822f977347eafe3cc1a6e
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2023-09-19 16:46:54 +0300

    add

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,7 +6,6 @@ export default function Projects() {
   return (
     <List>
       <Experience>
-       
         <Name>
           <Bold>[</Bold> HTML5, CSS3, SASS, BEM, PHP, JS, JQUERY, WordPress,
           ACF, WPML <Bold>]</Bold>
@@ -22,14 +21,12 @@ export default function Projects() {
       <Experience>
         <Link
           target="_blank"
-          href="https://oleksandrvasylchuk.github.io/coca-cola/"
-        >
+          href="https://oleksandrvasylchuk.github.io/coca-cola/">
           Coca-Cola
         </Link>
         <Link
           target="_blank"
-          href="https://oleksandrvasylchuk.github.io/coca-cola/"
-        >
+          href="https://oleksandrvasylchuk.github.io/coca-cola/">
           <AiFillGithub />
         </Link>
         <Name>
@@ -39,31 +36,12 @@ export default function Projects() {
         <Submenu>Own project. Business card site</Submenu>
       </Experience>
       <Experience>
-        <Link
-          target="_blank"
-          href="https://oleksandrvasylchuk.github.io/test-sayama/"
-        >
-          Sayama Real
-        </Link>
-        <Link
-          target="_blank"
-          href="https://github.com/OleksandrVasylchuk/test-sayama"
-        >
-          <AiFillGithub />
-        </Link>
-        <Name>
-          <Bold>[</Bold> HTML5, CSS3, JavaScript, GSAP.<Bold>]</Bold>
-        </Name>
-        <Submenu>Own project. Business card site</Submenu>
-      </Experience>
-      <Experience>
         <Link target="_blank" href="https://cabbage-project.netlify.app/">
           Kapusta
         </Link>
         <Link
           target="_blank"
-          href="https://github.com/virysik/goit-final-project-kapusta-front"
-        >
+          href="https://github.com/virysik/goit-final-project-kapusta-front">
           <AiFillGithub />
         </Link>
         <Name>
@@ -73,80 +51,16 @@ export default function Projects() {
         </Name>
         <Submenu>Team Project. Web app for calculation your finances.</Submenu>
       </Experience>
-      <Experience>
-        <Link target="_blank" href="https://sanya-phonebook.netlify.app/">
-          Phonebook
-        </Link>
-        <Link
-          target="_blank"
-          href="https://github.com/OleksandrVasylchuk/goit-react-hw-08-phonebook"
-        >
-          <AiFillGithub />
-        </Link>
-        <Name>
-          <Bold>[</Bold> HTML5, CSS3, JavaScript, React<Bold>]</Bold>
-        </Name>
-        <Submenu>
-          Own project. An application for storing contacts, that has
-          registration feature and can work with private collections of
-          contacts. Contacts are stored on the separate backend. The app is
-          written using React. State management is done using Redux. The app
-          consists of several pages. Routing is implemented.
-        </Submenu>
-      </Experience>
-      <Experience>
-        <Link target="_blank" href="https://sanyabatyushka-movies.netlify.app/">
-          Movies
-        </Link>
-        <Link
-          target="_blank"
-          href="https://github.com/OleksandrVasylchuk/goit-react-hw-05-movies"
-        >
-          <AiFillGithub />
-        </Link>
-        <Name>
-          <Bold>[</Bold> HTML5, CSS3, JavaScript, Axios, React.js, React router,
-          React hooks<Bold>]</Bold>
-        </Name>
-        <Submenu>
-          Own project. A movie finder application with the ability to view movie
-          information, cast and reviews. Routing is implemented. For the backend
-          is using themoviedb.org API. Asynchronous JavaScript code loading is
-          added.
-        </Submenu>
-      </Experience>
+
       <Experience>
         <Link
           target="_blank"
-          href="https://oleksandrvasylchuk.github.io/goit-react-hw-04-hooks-images/"
-        >
-          Image Finder
-        </Link>
-        <Link
-          target="_blank"
-          href="https://github.com/OleksandrVasylchuk/goit-react-hw-04-hooks-images"
-        >
-          <AiFillGithub />
-        </Link>
-        <Name>
-          <Bold>[</Bold> HTML5, CSS3, JavaScript, Axios<Bold>]</Bold>
-        </Name>
-        <Submenu>
-          Own project. An image finder application with the ability to view
-          images on a modal window. For the backend is using pixabay.com API
-        </Submenu>
-      </Experience>
-      <Experience>
-        <Link
-          target="_blank"
-          href="https://dmitry-karas.github.io/front-end__division--filmoteka/"
-        >
+          href="https://dmitry-karas.github.io/front-end__division--filmoteka/">
           Filmoteka
         </Link>
         <Link
           target="_blank"
-          href="https://github.com/Dmitry-Karas/front-end__division--filmoteka/tree/main"
-        >
+          href="https://github.com/Dmitry-Karas/front-end__division--filmoteka/tree/main">
           <AiFillGithub />
         </Link>
         <Name>
